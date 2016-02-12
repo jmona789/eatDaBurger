ce7bc4124979b19c1507a7bb50348f53fc2fddcc
jmona789 <jmona789@gmail.com>
2016-02-12 17:25:12 -0500

    added listen and require handlebars to server file

--- burger/server.js
+++ burger/server.js
@@ -1,3 +1,9 @@
 var express = require("express");
-var method = require("method-overide");
-var body = require("body-parser");
+var methodoverride = require("method-override");
+var bodyparser = require("body-parser");
+var app = express();
+var expressHandlebars = require('express-handlebars');
+
+app.listen(PORT, function() {
+  console.log('Listening on %s', PORT);
+});
