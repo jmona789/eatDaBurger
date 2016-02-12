5eb4ded716fbeacbb127f2c6027fecf38f9d112d
jmona789 <jmona789@gmail.com>
2016-02-12 17:19:05 -0500

    added more functions and fixed old function in orm file

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -2,14 +2,38 @@ var connectionImport = require("./connection.js");
 var mysql = require("mysql");
 
 
-connection.query("SELECT * FROM burgers", function(err, results) {
-  if(err) {
-    throw err;
-  }
-  var data = {
-    burgers: results
-  }
+module.exports = {
+  displayDevouredBurgers: function(){
+    connection.query("SELECT burger_name FROM burgers WHERE devoured = 1", function(err, results) {
+      if(err) {
+        throw err;
+      }
+      var data = {
+        burgers: results
+      }
 
-  console.log(data.burgers[0].burger_name);
-  //res.render('noteview', data);
-});
+      console.log(data.burgers);
+      //res.render('noteview', data);
+    });
+  },
+  displayNotDevouredBurgers: function(){
+    connection.query("SELECT burger_name FROM burgers WHERE devoured = 0", function(err, results) {
+      if(err) {
+        throw err;
+      }
+      var data = {
+        burgers: results
+      }
+
+      console.log(data.burgers);
+      //res.render('noteview', data);
+    });
+  },
+  addBurger: function(burger_name){
+    connection.query("INSERT INTO burgers (burger_name) VALUES(?)", burger_name, function(err, results) {
+      if(err) {
+        throw err;
+      }
+    });
+  }
+}
