2b860081069e4beddd18174e5e0e402a9b486811
jmona789 <jmona789@gmail.com>
2016-02-14 15:28:54 -0500

    fixed orm and finished model

--- burger/models/burger.js
+++ burger/models/burger.js
@@ -1,5 +1,22 @@
 var orm = require("../config/orm.js");
 
-module.exports = {
-  
-}
+var burgers = {
+  addBurger: function(nameInput, cb){
+    orm.insertOne("burgers", nameInput, cb);
+  },
+  devourBurger: function(inputId, cb){
+    orm.updateOne("burgers", inputId, cb);
+  },
+  findDevoured: function(cb) {
+    orm.selectDevoured("burgers", function(res){
+      cb(res);
+    });
+  },
+  findNotDevoured: function(cb) {
+    orm.selectNotDevoured("burgers", function(res){
+      cb(res);
+    });
+  },
+};
+
+module.exports = burgers
