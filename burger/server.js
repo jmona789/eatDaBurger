5858b412281a7ad54e354efe56e1110b9515de98
jmona789 <jmona789@gmail.com>
2016-02-14 17:51:27 -0500

    removed inline styling and serve them via static folder

--- burger/server.js
+++ burger/server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var app = express();
 
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use("/static", express.static("public"));
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({
